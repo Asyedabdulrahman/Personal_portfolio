3979465626d27753c82ec65daae77b7aa8bac8df
unknown <syed@gmail.com>
2024-09-25 01:22:45 +0530

    added overflow  x clip in hero.tsx file

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,7 +8,10 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-50 relative z-0" id="heroSection">
+    <div
+      className="py-32 md:py-48 lg:py-50 relative z-0 overflow-x-clip"
+      id="heroSection"
+    >
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_1%,black_68%,transparent)]">
         <div
           className="absolute inset-0 -z-30 opacity-5"
@@ -16,12 +19,11 @@ export const HeroSection = () => {
             backgroundImage: `url(${grainImage.src})`,
           }}
         ></div>
-        <div className="hero-ring size-[700px]"></div>
-        <div className="hero-ring size-[900px]"></div>
-        <div className="hero-ring size-[1100px]"></div>
-        <div className="hero-ring size-[1300px]"></div>
+        <div className="hero-ring size-[500px] lg:size-[700px]"></div>
+        <div className="hero-ring size-[600px] lg:size-[900px]"></div>
+        <div className="hero-ring size-[800px] lg:size-[1100px]"></div>
         <HeroOrbit
-          size={550}
+          size={690}
           rotation={0}
           shouldOrbit={true}
           orbitDuration="60s"
@@ -31,7 +33,7 @@ export const HeroSection = () => {
           <StarIcon className="size-12 text-emerald-300" />
         </HeroOrbit>
         <HeroOrbit
-          size={590}
+          size={810}
           rotation={98}
           shouldOrbit={true}
           orbitDuration="65s"
@@ -81,7 +83,7 @@ export const HeroSection = () => {
           <SparkleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
-          size={560}
+          size={520}
           rotation={25}
           shouldOrbit={true}
           orbitDuration="83s"
@@ -90,13 +92,43 @@ export const HeroSection = () => {
         >
           <SparkleIcon className="size-5 text-emerald-300" />
         </HeroOrbit>
+        <HeroOrbit
+          size={515}
+          rotation={285}
+          shouldOrbit={true}
+          orbitDuration="83s"
+          shouldSpin={true}
+          spinDuration="70s"
+        >
+          <SparkleIcon className="size-5 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={665}
+          rotation={255}
+          shouldOrbit={true}
+          orbitDuration="83s"
+          shouldSpin={true}
+          spinDuration="70s"
+        >
+          <SparkleIcon className="size-7 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={817}
+          rotation={265}
+          shouldOrbit={true}
+          orbitDuration="83s"
+          shouldSpin={true}
+          spinDuration="70s"
+        >
+          <SparkleIcon className="size-10 text-emerald-300" />
+        </HeroOrbit>
       </div>
 
-      <div className="container">
+      <div className="container max-w-screen-xl mx-auto px-4">
         <div className="flex flex-col items-center">
           <Image
             src={memojiImage}
-            className="size-[100px]"
+            className="w-24 h-24 md:w-32 md:h-32"
             alt="person sitting with laptop"
           />
           <div className="gap-2 bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center rounded-lg">
@@ -114,11 +146,11 @@ export const HeroSection = () => {
             Building Exceptional user experiences
           </h1>
           <p className="mt-4 text-center text-white/60 capitalize md:text-lg">
-            i specialise in transforming design into functional,high-perfomant
-            web applications, lets discuss your next project
+            I specialise in transforming design into functional,
+            high-performance web applications, lets discuss your next project
           </p>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 flex-wrap">
           <button className="inline-flex gap-2 items-center border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown classname="size-6" />
