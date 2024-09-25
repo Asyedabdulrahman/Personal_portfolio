b1ad2b3dc62c0e7b85944652f8b54d2e1f13bd80
unknown <syed@gmail.com>
2024-09-25 23:19:28 +0530

    done some  modification

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -134,7 +134,7 @@ export const AboutSection = () => {
             <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
               <CardHeader
                 title="Beyond the code"
-                description="Play with Drag and Drop ⬇️🔻"
+                description="Play with Drag and Drop🔻"
                 className="px-6 py-6 "
               />
               <div className="relative flex-1" ref={constraintRef}>
